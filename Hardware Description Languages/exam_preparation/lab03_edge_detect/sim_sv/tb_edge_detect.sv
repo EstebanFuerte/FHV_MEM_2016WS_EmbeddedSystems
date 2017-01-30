90f1cd9e74ad34e4399706022256dd2e73dab51a
Stefan Stark <sst5281@students.fhv.at>
2017-01-30 15:26:32 +0100

    upload lab03

--- Hardware Description Languages/exam_preparation/lab03_edge_detect/sim_sv/tb_edge_detect.sv
+++ Hardware Description Languages/exam_preparation/lab03_edge_detect/sim_sv/tb_edge_detect.sv
@@ -1,52 +1,61 @@
 // ------------------------------------
 // MEM3 -- HDL
-// Project:
+// Project:		Test for edge detect
 // Author:		stst
 // Date:		30.01.2017
 // ------------------------------------
 
-module tb_sevenseg();
+`timescale 10ns/10ns        // Tell the simulator what the minimum time step shall be
+
+module tb_edge_detect();
 
 	// (1) Create witing to connect the DUT
-/* 	For Simulations with clks
 	logic       rst_n;
-    logic       clk50m; */
+    logic       clk50m;
+	logic		in;
+	logic		risingedge;
+	logic		fallingedge;
 	
 	// (2) Plug in the DUT
-	sevenseg dut
+	edge_detect dut
 	(
 		.*	//possible if they have the same name and type
 	);
 	
 	// (3) Drive the DUT inputs using stimuli
-	
-/* 	For Simulations with clks
 	logic run_sim = 1'b1;
 	initial begin : clk_gen
 		clk50m = 1'b0;
 		while (run_sim)begin
-			#10ns clk50m = ~ clk50m;
+			#10ns;
+			clk50m = ~ clk50m;
 		end
-	end*/
+	end
 	
 	// General
 	initial begin
 		$display("-------------------");
 		$display("tb_sevenseg started");
 		$display("-------------------");
+		rst_n = 1'b0;
+		in = 1'b1;
 		
 		// End POR (power on reset)
-		/* 	For Simulations with clks
 		#99ns;			// Wait for 99ns (asynchronous to the clock)
         rst_n = 1'b1;   // Release the rst_n, dut leaves reset
-        */
 		
+		// create a few edges
+		for (int i=0; i<10; i++) begin
+            #1us;                   // Wait for 1us
+            @ (negedge clk50m)      // Wait for the falling edge of the clock
+                                    // >>> Change inputs at the passive edge of the clock <<<
+            in = ~in;               // Toggle the signal 'in'
+            $display("Input in is now %d",in);
+		end
 		
 		// Stop simulation
 		#1us;
-		/* 	For Simulations with clks
 		run_sim = 1'b0;             // Stops the clk_gen while loop
-		*/
 		
 		$display("-------------------");
 		$display("tb_sevenseg finished");
--- Hardware Description Languages/exam_preparation/template_src.sv
+++ Hardware Description Languages/exam_preparation/template_src.sv
@@ -9,11 +9,51 @@ module sevenseg(
 	// Define inputs and outputs
 );
 
+	// place for wires
+	//logic	in_ff1, in_ff2;
+	
+	// Combinatorial logic
 	always_comb begin
-		
+		// describe complex combinatorial logic
+		// if, case for or while can be used
+		// Output signals need to be defined for all conditions
 	end
 	
-	// outside an always - assign is necessary
 	
+	// Sequential logic
+	// To describe sequential logic (FFs, counters)
+	// Only changes o fthe output signal needs to specified
+	// D-FF:
+	always_ff @ (negedge rst_n or posedge clk50m) begin : d_ff_1
+			if (!rst_n) begin		// async reset (active low)
+				q <= 1'b0;			// reset value
+			end
+			else begin				// synchronous behaviour
+				q <= d;				// at rising edge of the clk, q follows d
+			end
+	end: d:ff1
+	
+	// 4bit Counter
+	always_ff @ (negedge rst_n or posedge clk50m) begin : 4bit_up_cnd
+		if(!rst_n) begin			// async reset (avtive low)
+			cnt <= '0;				// '0 is all zeros
+		end
+		else if (load) begin
+			cnt <= data_in;
+		end
+		else if(en) begin
+			cnt <= cnt +4'd1;
+		end
+		// no else is requiered here
+	
+	end: 4bit_up_cnd
+	
+	// outside an always - assign is necessary
+	// connection of submodules
+	// connection of other signals
+	// concentration of signals
+	// Simple logic operations
+	// assign risingedge  =  in_ff1 & ~in_ff2;
+    // assign fallingedge = ~in_ff1 &  in_ff2;
 
 endmodule
